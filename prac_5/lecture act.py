dc8b1cc8f0116b30a585c3788886d2d9432b9fcb
ashwin <Ashwinunmesh@gmail.com>
2019-09-11 11:45:58 +1000

    update

--- prac_5/lecture act.py
+++
@@ -1,7 +0,0 @@
-products = [["phone",340 , False],["pc",1420.95, True],["plant",24.5,True]]
-for product in products:
-    sections = product.strip().split(',')
-    if sections[3] == True:
-        print("Yaya")
-
-
